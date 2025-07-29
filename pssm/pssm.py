c1bbaefbe68af9108075f2520f817494fb3a921c
hayden-johnson <haydenjohnson054@gmail.com>
2025-07-29 14:52:17 +0100

    update pssm

--- pssm/pssm.py
+++ pssm/pssm.py
@@ -1,6 +1,7 @@
 from pssm.utils import cumulative_spike_matrix
 import torch
 import torch.nn as nn
+from scipy.stats import entropy
 
 class PoissonSSM:
     def __init__(self, encoder, decoder):
@@ -16,10 +17,20 @@ class PoissonSSM:
             sample, indicator, times = dist.rsample(hard=True, return_indicator=True)
             # convert to cumulative spike matrix
             cumulative_matrix = cumulative_spike_matrix(indicator, times, n_steps=n_timesteps)
+            
+            print("Cumulative matrix shape:", cumulative_matrix.shape)
+            
+            
+            
             # decode each col into a decoder posterior/entropy
-            entropies = [decoder[x] for x in cumulative_matrix.T]
+            logits = [self.decoder(x.float().to(device)) for x in cumulative_matrix.T]
+            
+            print("Logits shape:", logits)
+            
+            probs = [nn.functional.softmax(logit, dim=-1) for logit in logits]
+            entropies = [entropy(prob.detach().cpu().numpy()) for prob in probs]
+            
             # decoder posterior/entropy at each timestep
             # get time, action, and entropy trace
         # return results
-        pass
-
+        return cumulative_matrix, logits, probs, entropies
